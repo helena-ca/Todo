825b2cae1f424116bc2c9bd09ba36606e232e07a
Helena Andrade <helena.carolina.andrade@gmail.com>
2025-09-14 15:44:49 +0100

    Added functionality, now able to schedule and update the task list

--- schema.sql
+++ schema.sql
@@ -2,23 +2,21 @@
 CREATE TABLE IF NOT EXISTS Tasks(
     id INTEGER,
     name TEXT NOT NULL UNIQUE,
+    start_date TEXT ,
     --Reccurence: Does it repeat?
     rec INTEGER DEFAULT 0,
-    --The time the task takes
-    dur INTEGER NOT NULL,
-    --The date the task starts
-    start_date TEXT ,
+    --If it repeats, does it repeat on a specific weekday
+    wk INTEGER,
     PRIMARY KEY (id),
-    CHECK (rec IN (0,1))
-    CHECK (dur > 0)
+    CHECK (rec IN (0,1)),
+    CHECK (dur > 0),
+    CHECK (-1<wk<7)
 );
 
 --List of tasks in a given day
 CREATE TABLE IF NOT EXISTS Listicle(
     id INTEGER,
     task_id INTEGER NOT NULL,
-    status INTEGER ,
     PRIMARY KEY (id),
     FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE,
-    CHECK (status IS NULL or status IN (0,1))
 );
--- tasks.py
+++ tasks.py
@@ -1,5 +1,6 @@
 import sqlite3
 import argparse
+import datetime
 
 
 def start_db (schema):
@@ -13,42 +14,82 @@ def start_db (schema):
     conn.commit()
     return conn
 
+def valid_date(pot_date):
+    try:
+        return datetime.datetime.strptime(pot_date, "%Y-%m-%d").date()
+    except:
+        raise argparse.ArgumentTypeError(f"Not a valid date: {pot_date}. Use format YYYY-MM-DD.")
+
+def valid_taskname(maybe_name):
+    name = conn.execute("SELECT id FROM Tasks WHERE id=?", (maybe_name, )) 
+    if name :
+        return maybe_name
+    else:
+        raise argparse.ArgumentTypeError(f"Not a valid task name: {maybe_name}. Try adding that task or ensuring it is well spelled.")
+
 def set_parser():
     parser = argparse.ArgumentParser()
     sub = parser.add_subparsers(dest="cmd", required = True)
 
     p_add_task = sub.add_parser("add_task", help = "Plan a new task for the future")
     p_add_task.add_argument("name", help= "Name of the task")
-    p_add_task.add_argument("-r", "--recurring", action = "store_true" ,help= "Whether it repeats or not")
-    p_add_task.add_argument("dur", type=int ,help= "How much time the task takes to complete")
-    p_add_task.add_argument("-d", "--date" ,help= "When does this task start")
+
+    p_schd = sub.add_parser("Schd_task", help = "Schedule a task once or ciclically")
+    p_add_task.add_argument("name", type= valid_taskname, help= "Name of the task")
+    p_schd.add_argument("date", type = valid_date, help= "When does this task starts or happens in YYYY-MM-DD format")
+    p_schd.add_argument("-r", "--recurring", action = "store_true", help= "Whether it repeats or not")
+    p_schd.add_argument("-wk", type=int ,help= "What day of the week does this task reccur in")
 
     p_list_task = sub.add_parser("list_tasks", help= "Provides list of tasks for today")
 
     return parser
 
-def add_task(conn, name, rec, dur, date):
-    conn.execute("INSERT INTO Tasks (name, rec, dur, start_date) VALUES (?, ?, ?, ?)", (name, rec, dur, date))
+def add_task(conn, name):
+    conn.execute("INSERT INTO Tasks (name) VALUES (?)", (name))
     conn.commit()
     print(f"The task {name} has been registered")
 
 def list_task(conn):
+    list_update(conn)
     today_todo = conn.execute("""
         SELECT t.name
         FROM Listicle AS l
         INNER JOIN Tasks AS t ON t.id = l.task_id
-        ORDER BY t.name
+        ORDER BY t.name;
     """)
     for todo in today_todo:
         print(todo["name"])
 
+def list_update(conn):
+    conn.execute("TRUNCATE TABLE Listicle;")
+    cur = conn.execute("SELECT * FROM Tasks")
+    now = datetime.datetime.now()
+    for row in cur:
+        if row["start_date"] == now:
+            conn.execute ("INSERT INTO Listicle (task_id) VALUES (?)", (row["id"]))
+        elif row["start_date"] < now:
+            if row["rec"]:
+                if not row["wk"]:
+                    conn.execute ("INSERT INTO Listicle (task_id) VALUES (?)", (row["id"]))
+                else:
+                    if row["wk"]==now.weekday:
+                        conn.execute ("INSERT INTO Listicle (task_id) VALUES (?)", (row["id"]))
+    conn.commit()
+
+def schd_task(conn,name, date, rec, wk):
+    conn.execute("UPDATE Tasks SET start_date = ?, rec = ?, wk = ? WHERE name = ?", (date, rec, wk, name))
+    conn.commit()
+    print(f"The task {name} has been registered")
 
 parser = set_parser()
 args = parser.parse_args()
 
-conn= start_db
+conn= start_db("schema.sql")
+
 
 if args.cmd =="add_task":
-    add_task(conn, args.name, args.r, args.dur, args.date)
+    add_task(conn, args.name)
+elif args.cmd =="schd_task":
+    schd_task(conn, args.name, args.date, args.r, args.wk)
 elif args.cmd =="list_tasks":
     list_task(conn)
